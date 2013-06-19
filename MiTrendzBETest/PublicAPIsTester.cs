79eaaffa6f896e4868bd54a796a0bd7d06c8dd02
Mohamed F. Ahmed <Mohamed.Ahmed@engr.uconn.edu>
2013-06-19 16:44:09 -0700

    initial checkin

---
+++ MiTrendzBETest/PublicAPIsTester.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiTrendzBETest
+{
+    public class PublicAPIsTester
+    {
+        public ObjectId
+        public PublicAPIsTester()
+        {
+        }
+        public StartTesting()
+        {
+        }
+    }
+}
